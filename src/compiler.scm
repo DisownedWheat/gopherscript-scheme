1f320f0b26515793f75b2055c7e802bad484a501
Rob Larnach <robert.larnach@gmail.com>
2017-03-29 11:39:32 +1100

    More work on parser. Begin adding recognition for more tokens in AST

--- src/compiler.scm
+++ src/compiler.scm
@@ -11,8 +11,8 @@ let y = (z) -> {
 (define (main)
   (print test-input)
   (define tokens (tokeniser test-input))
-  (print tokens)
-  (print (parser tokens))
-  (map (lambda (x) (print (token-type x) " " (token-value x))) tokens))
+  (map (lambda (x) (print (token-type x) " " (token-value x))) tokens)
+  (define ast (parser tokens))
+  (print (ast-node-body ast)))
 
 (main)
--- src/parser.scm
+++ src/parser.scm
@@ -2,26 +2,40 @@
 (require-extension defstruct srfi-13  srfi-1)
 (use loops utf8 matchable)
 
-(declare (unit parser))
-(declare (uses lexer))
+(declare (unit parser) (uses lexer))
 
 ; Define structs
-(defstruct ast-node type body)
-
-; Instantiate index of tokens
-(define i 0)
-(define (iinc)
-  (set! i (+ i 1)))
-
-(define (walk acc tok)
- (define type (token-type tok))
- (print (token-value tok))
- (match type
-     ("NEWLINE" (cons acc (make-ast-node type: "NEWLINE" body: '())))
-     (_ acc)))
+(defstruct ast-node type body value)
 
 (define (parser toks)
-  (define ast (make-ast-node type: "Program" body: '()))
+ ; Instantiate index of tokens
+ (define i 0)
+ (define iinc (lambda () (set! i (+ i 1))))
 
-  (define body (fold walk '() toks))
-  (make-ast-node type: "Program" body: body))
+ (define walk
+   (lambda ()
+    (define node (list-ref toks i))
+    (print "NODE IS: " (token-value node))
+    (define type (token-type node))
+    (set! body
+     (append body
+      `(,(match type
+          ("NEWLINE" (make-ast-node type: "NEWLINE" body: '() value: "\n"))
+          ("LPAREN" (begin
+                     (iinc)
+                     (make-ast-node type: "PARENLIST" value: ""
+                      body: (let ((node (list-ref toks i)))
+                             (do-until (string=? (token-type node) "RPAREN")
+                              (walk))))))
+          (_ (make-ast-node type: "GENERIC" body: '() value: (token-value node)))))))
+    (iinc)))
+
+ (define body '())
+ (do-until (= i (length toks))
+  (walk))
+ (make-ast-node type: "Program" body: body))
+
+(define (peek toks i) (list-ref toks (+ i 1)))
+(define (peek-n toks i n) (list-ref toks (+ i n)))
+(define (check toks i) (list-ref toks (- i 1)))
+(define (check-n toks i n) (list-ref toks (- i n)))
