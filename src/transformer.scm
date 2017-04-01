7e640993f357e21ca348e0641baf8130869f90f6
Rob Larnach <robert.larnach@gmail.com>
2017-04-02 01:17:29 +1100

    Trying to take a more functional approach to the transformer

--- src/transformer.scm
+++ src/transformer.scm
@@ -5,16 +5,19 @@
 (declare (unit transformer) (uses parser))
 
 (define (transformer ast)
-  (define i 0)
-  (define (iinc)
-    (set! i (+ i 1)))
-
   (define (traverse ast parent)
+   (let ((i 0))
     (map
       (lambda (node)
+        (set! i (+ i 1))
         (match (ast-node-type node)
-          ("ASSIGN" '())))
-      ast))
+          ("ASSIGN" (if (member (ast-node-type (peek-n ast i 2)) '("FUNCDEF" "PARENLIST"))
+                      (let ((paren-body (traverse (peek ast i) node))
+                            (func-body (travers (p))))
+                        (make-ast-node type: "ASSIGNFUNC" body: body value: (ast-node-value node)))))
+          ("PARENLIST" (if (string=? (ast-node-type (peek ast i)) "FUNCDEF")
+                        '()))))
+      ast)))
 
   (define body '())
   (do-until (= i (length (ast-node-body ast)))
